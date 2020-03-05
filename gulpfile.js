ace947e549804753c4a620e67745f0afb1ee7c7a
Vladimir0x29A <vladimir0x29a@gmail.com>
2020-03-05 17:59:27 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const {src, dest, watch, parallel, series} = require('gulp'),
+      autoprefixer = require('gulp-autoprefixer'),
+      connect =   require('gulp-connect'),
+      sass =      require('gulp-sass'),
+      rename =    require('gulp-rename'),
+      sourceMaps = require('gulp-sourcemaps'),
+      stripComments = require('gulp-strip-comments'),
+      concatCss = require('gulp-concat-css');
+
+function connectTask(done) {
+    connect.server({
+        root: 'build',
+        port: 8090,
+        livereload: true
+    }, function () {
+        this.server.on('close', done);
+    });
+}
+
+function reloadTask(done) {
+    src('src/views/index.html')
+        .pipe(stripComments.html())
+        .pipe(dest('build'))
+        .pipe(connect.reload());
+    done();
+}
+
+function jsTask(done) {
+    src('scripts-source/scripts.js')
+        .pipe(dest('build/scripts'))
+        .pipe(connect.reload());
+    done();
+}
+
+function cssTask() {
+    return src('src/style/main.scss')
+        .pipe(sourceMaps.init())
+        .pipe(sass().on('error', sass.logError))
+        /*.pipe(autoprefixer({
+           browsers: ['last 15 versions'],
+           cascade: false
+        }))*/
+        .pipe(sourceMaps.write())
+        .pipe(rename('main.css'))
+        .pipe(dest('build'))
+        // .pipe(dest('style-source/css'));
+        .pipe(connect.reload());
+}
+
+function concatCssTask() {
+    return src(['style-source/css/orig.css', 'style-source/css/main.css'])
+        .pipe(concatCss("style.css"))
+        .pipe(dest('build'))
+        .pipe(connect.reload());
+}
+
+function watchTask() {
+    watch(['src/style/*.scss'], cssTask);
+    // gulp.watch('scripts-source/!*.js', ['js']);
+    // gulp.watch('build/img/!*.*', ['reload']);
+    // gulp.watch('build/fonts/!*.*', ['reload']);
+    watch(['src/views/index.html'], reloadTask);
+    // watch('scripts-source/scripts.js', jsTask);
+}
+
+exports.style = cssTask;
+exports.default = parallel(connectTask, reloadTask, cssTask, watchTask);
